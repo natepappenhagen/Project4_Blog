16a61418cc27efa72b56009186e12523702faf4c
Nate Pappenhagen <natepappenhagen@gmail.com>
2018-08-23 15:01:51 -0600

    set procfiles

--- blog_django/settings.py
+++ blog_django/settings.py
@@ -136,3 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
